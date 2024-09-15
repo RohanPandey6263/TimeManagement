55282bbc093d9426b8d468a14f650de125ec4d05
RohanPandey6263 <pandey.rohan673@gmail.com>
2024-09-15 18:36:52 -0500

    added timer

--- src/App.js
+++ src/App.js
@@ -2,6 +2,11 @@ import logo from './logo.svg';
 import './App.css';
 import clocklogo from './favicon.ico';
 function App() {
+
+  const today = new Date();
+  console.log(today.getTime());
+  console.log(today.toLocaleTimeString());
+  const timenow = today.toLocaleTimeString();
   return (
     <div style={{backgroundColor:'#2f70d1', verticalAlign:'stretch', height:'450px', width:'800px'}} className="App">
       <header className="App-header" style={{marginLeft:'10px'}}>     
@@ -15,7 +20,9 @@ function App() {
         <div style={{ display: 'flex',  height:'100px'}} >
           <div className="lightbox-div">
             <div className= "lightbox-div" style={{width:'210px',flexDirection:'column'}}>
-              <div className="Box-div" style={{height:'50px', width:'100%'}}></div>
+              <div className="Box-div" style={{height:'50px', width:'100%'}}>
+                <p>{timenow}</p>
+              </div>
               <div className="lightbox-div" style={{height:'50px', width:'100%'}}></div>
             </div>
             <div className= "Box-div" style={{width:'430px', height:'100%', marginLeft:'20px'}}>ah</div>
