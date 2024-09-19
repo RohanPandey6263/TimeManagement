a89187921fca5a61f637de939587b33e25236b7e
RohanPandey6263 <pandey.rohan673@gmail.com>
2024-09-19 15:02:16 -0500

    timer almost done

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ import {
   Alert,
 } from 'react-native';
 function App() {
-  let timerOn = false;
+  let timerOn = true;
   function isTimerOn()
   {
     console.log(timerOn);
@@ -32,7 +32,8 @@ function App() {
       }
     setTimenow((timenow)=>{return new Date()});
   };
-  // const today = new Date();
+  const today = new Date();
+  console.log(today.toLocaleDateString("en-US", { weekday: 'long' }))
   const [seconds, setSeconds] = useState(0);
   const [timenow, setTimenow] =useState(new Date());
   //const [timerOn, setTimerOn] = useState(true);
@@ -44,9 +45,10 @@ function App() {
   
   // console.log(today.getTime());
   // console.log(today.toLocaleTimeString());
-  const schedules = fetchUpcomingSchedules();
+  const days = fetchUpcomingdays();
   //fetchUpcomingSchedulesFromServer();
-  console.log(schedules);
+  console.log(days);
+  const schedules = fetchUpcomingSchedules();
   function pad2(number) {
     return (number < 10 ? '0' : '') + number
   }
@@ -107,14 +109,40 @@ function App() {
             
             </div>
           </div> 
-        </div>
-        <div className="Box-div" style={{marginTop:'10px',height:'30px',marginRight:'10px'}}>Calendar</div>
-        <div className="Box-div" style={{height:'170px', marginRight:'10px',marginTop:'10px'}}>hi</div>
+         </div>
+          <div className="Box-div" style={{marginTop:'10px',height:'30px',marginRight:'10px'}}>Calendar</div>
+          <div className="lightbox-div" style={{height:'170px', marginRight:'10px',marginTop:'10px'}}>
+            {days.slice(0,7).map(day => 
+            <div className="Box-div" style={{height:'95%',width:'105px',marginLeft:'5px',fontSize:'12px',display:'flex',flexDirection:'column'}}>
+              <div>{day.dotw}</div>
+              <div>{day.dotm}</div>
+            </div>
+            )
+          }
+          </div>
+       
       </body>
     </div>
   );
 }
 
+function fetchUpcomingdays() {
+  const day1 = new Date();
+  const day2 = new Date();
+  day2.setDate(day1.getDate()+1)
+  const day3 = new Date(day1.getDate()+2);
+  const day4 = new Date(day1.getDate()+3);
+  const day5 = new Date(day1.getDate()+4);
+  const day6 = new Date(day1.getDate()+5);
+  const day7 = new Date(day1.getDate()+6);
+  return [{dotw:day1.toLocaleDateString("en-US", { weekday: 'long' }), dotm:day1.getDate()},
+    {dotw:'Chemistry Assignment', dotm:day2.getDate()} ,
+    {dotw:'CS Assignment', dotm:day3.getDate()},
+    {dotw:'Biology Assignment', dotm:day4.getDate()},
+    {dotw:'Math Assignment', dotm:day5.getDate() },
+    {dotw:'Sem Assignment', dotm:day6.getDate() },
+    {dotw:'English Assignment', dotm:day7.getDate()}];
+}
 function fetchUpcomingSchedules() {
   return [{description:'Physics Assignment', dueBy:new Date('September 17, 2024 03:24:00')},
     {description:'Chemistry Assignment', dueBy:new Date('December 17, 1995 03:24:00')} ,
