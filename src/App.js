e268d03536b982078c26daf4b8b9ed592d258aa7
RohanPandey6263 <pandey.rohan673@gmail.com>
2024-09-20 22:10:36 -0500

    styling done

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import logo from './logo.svg';
 import './App.css';
-import clocklogo from './favicon.ico';
+import clocklogo from './clockicon.png';
+import playicon from './playiconn.png'
+import pauseicon from './pauseiconn.png'
+import reseticon from './reseticon.png'
 import axios from 'axios';
 import { useState, useEffect } from 'react';
 import {
@@ -11,8 +14,9 @@ import {
   Text,
   Alert,
 } from 'react-native';
+let timerOn = false;
 function App() {
-  let timerOn = true;
+ 
   function isTimerOn()
   {
     console.log(timerOn);
@@ -60,7 +64,7 @@ function App() {
         
         <img src={clocklogo} style={{height:'30px'}}> 
         </img>  
-        <strong style={{marginLeft:'10px'}}>
+        <strong style={{marginLeft:'10px',fontFamily:'Myriad Pro Regular'}}>
           Time Crunch
         </strong> 
         <div className= "rounded-div" style={{height:'20px',width:'70px',fontSize:'12px',padding:'5px',marginLeft:'470px'}}> 
@@ -75,10 +79,16 @@ function App() {
               <div style={{display:'flex',flexDirection:'row',marginTop:'5px'}}>
               
                 
-                <button className="circle-div" style={{height:'50px', width:'50px',marginLeft:'0px' }}  onClick={() => {
-                   timerOn = true;
-                  }}>P</button>
-                <button className="circle-div" style={{height:'50px', width:'50px'}} onClick={() => setSeconds((seconds)=>{return 0})}>R</button>
+                <button className="circle-div" style={{height:'50px', width:'50px',marginLeft:'0px', }}  onClick={() => {
+                   if (timerOn == true) {
+                    timerOn = false;
+                    document.getElementById('theImage').src=playicon; 
+                   } else if (timerOn == false) {
+                    timerOn = true; 
+                    document.getElementById('theImage').src=pauseicon; 
+                   }
+                  }}><img id="theImage" src={playicon} style={{height:'30px'}}></img></button>
+                <button className="circle-div" style={{height:'50px', width:'50px'}} onClick={() => setSeconds((seconds)=>{return 0})}><img src={reseticon} style={{height:'40px'}}></img></button>
                 <div style={{display:'flex',flexDirection:'column'}}>
                  <button className="rounded-div" onClick={() => setSeconds((seconds)=>{
 
@@ -97,12 +107,12 @@ function App() {
               </div >
               <div className="lightbox-div" style={{height:'50px', width:'100%'}}></div>
             </div>
-            <div className= "topBox-div" style={{width:'430px', height:'100%', marginLeft:'20px',flexDirection:'column'}}>
-              <div  style={{marginTop:'10px',height:'20px',fontSize:'17px'}}>
+            <div className= "topBox-div" style={{width:'530px', height:'100%', marginLeft:'20px',flexDirection:'column'}}>
+              <div  style={{marginTop:'10px',height:'20px',fontSize:'17px',fontFamily:'Times New Roman'}}>
                 Upcoming
                 <div className="Box-div" style={{height:'80px', width:'100%', flexDirection:'row',marginTop:'10px',}}>
                   {schedules.sort((a,b)=>a.dueBy-b.dueBy).slice(0,5).map(sch => 
-                    <div className="lightbox-div" style={{height:'74px',width:'76px',marginLeft:'5px',fontSize:'12px'}}>{sch.description}</div>
+                    <div className="lightbox-div" style={{height:'88px',width:'90px',marginLeft:'8px',fontSize:'12px'}}>{sch.description}</div>
                   )
                 }
                 </div>
@@ -110,11 +120,11 @@ function App() {
             </div>
           </div> 
          </div>
-          <div className="Box-div" style={{marginTop:'10px',height:'30px',marginRight:'10px'}}>Calendar</div>
-          <div className="lightbox-div" style={{height:'170px', marginRight:'10px',marginTop:'10px'}}>
+          <div className="Box-div" style={{marginTop:'10px',height:'30px',marginRight:'10px',fontFamily:'Brush Script MT'}}>Calendar</div>
+          <div className="toplightbox-div" style={{height:'170px', marginRight:'10px',marginTop:'10px'}}>
             {days.slice(0,7).map(day => 
-            <div className="Box-div" style={{height:'95%',width:'105px',marginLeft:'5px',fontSize:'12px',display:'flex',flexDirection:'column'}}>
-              <div>{day.dotw}</div>
+            <div className="topBox-div" style={{height:'95%',width:'105px',marginLeft:'5px',fontSize:'12px',display:'flex',flexDirection:'column'}}>
+              <div style={{marginTop:'10px'}}>{day.dotw}</div>
               <div>{day.dotm}</div>
             </div>
             )
@@ -130,19 +140,26 @@ function fetchUpcomingdays() {
   const day1 = new Date();
   const day2 = new Date();
   day2.setDate(day1.getDate()+1)
-  const day3 = new Date(day1.getDate()+2);
-  const day4 = new Date(day1.getDate()+3);
-  const day5 = new Date(day1.getDate()+4);
-  const day6 = new Date(day1.getDate()+5);
-  const day7 = new Date(day1.getDate()+6);
-  return [{dotw:day1.toLocaleDateString("en-US", { weekday: 'long' }), dotm:day1.getDate()},
-    {dotw:'Chemistry Assignment', dotm:day2.getDate()} ,
-    {dotw:'CS Assignment', dotm:day3.getDate()},
-    {dotw:'Biology Assignment', dotm:day4.getDate()},
-    {dotw:'Math Assignment', dotm:day5.getDate() },
-    {dotw:'Sem Assignment', dotm:day6.getDate() },
-    {dotw:'English Assignment', dotm:day7.getDate()}];
-}
+  const day3 = new Date();
+  day3.setDate(day1.getDate()+2)
+  const day4 = new Date();
+  day4.setDate(day1.getDate()+3)
+  const day5 = new Date();
+  day5.setDate(day1.getDate()+4)
+  const day6 = new Date();
+  day6.setDate(day1.getDate()+5)
+  const day7 = new Date();
+  day7.setDate(day1.getDate()+6)
+  
+    return [
+      {dotw:day1.toLocaleDateString("en-US", { weekday: 'long' }), dotm:day1.getDate()},
+      {dotw:day2.toLocaleDateString("en-US", { weekday: 'long' }), dotm:day2.getDate()} ,
+      {dotw:day3.toLocaleDateString("en-US", { weekday: 'long' }), dotm:day3.getDate()},
+      {dotw:day4.toLocaleDateString("en-US", { weekday: 'long' }), dotm:day4.getDate()},
+      {dotw:day5.toLocaleDateString("en-US", { weekday: 'long' }), dotm:day5.getDate() },
+      {dotw:day6.toLocaleDateString("en-US", { weekday: 'long' }), dotm:day6.getDate() },
+      {dotw:day7.toLocaleDateString("en-US", { weekday: 'long' }), dotm:day7.getDate()}];
+  }
 function fetchUpcomingSchedules() {
   return [{description:'Physics Assignment', dueBy:new Date('September 17, 2024 03:24:00')},
     {description:'Chemistry Assignment', dueBy:new Date('December 17, 1995 03:24:00')} ,
